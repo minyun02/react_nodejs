6bd9cc0046d844fad999b59b8b9972022e0105d0
min <myun02302@gmail.com>
2023-06-26 14:57:15 +0900

    댓글 지우기 기능 구현

--- backend/middlewares/AuthMiddleware.js
+++ backend/middlewares/AuthMiddleware.js
@@ -1,8 +1,9 @@
 const {verify} = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
+  
   const accessToken = req.header("accessToken");
-
+  
   if(!accessToken) return res.json({error: "User not logged in"});
 
   try {
