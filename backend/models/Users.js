ba16cdece775744bd065eca81ddeddcc1e86adf9
min <myun02302@gmail.com>
2023-07-02 22:58:01 +0900

    좋아요 기능 구현

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -12,7 +12,11 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   Users.associate = (models) => {
-    Users.hasMany(models.Posts, {
+    // Users.hasMany(models.Posts, {
+    //   onDelete: "cascade",
+    // });
+
+    Users.hasMany(models.Likes, {
       onDelete: "cascade",
     });
   }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,22 +6,81 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
 
   const [ListOfPosts, setListOfPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+
   let navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
+    axios.get("http://localhost:3001/posts",
+    { headers: {
+      accessToken: localStorage.getItem("accessToken")
+      }
+    }).then((response) => {
+      setListOfPosts(response.data.listOfPosts);
+      setLikedPosts(response.data.likedPosts.map((like) => {
+        return like.PostId;
+      }));
     });
   }, []);
+
+  const likeAPost = (postId) => {
+    axios.post(
+      "http://localhost:3001/likes",
+      { PostId: postId }, 
+      { headers: {
+        accessToken: localStorage.getItem("accessToken")
+        }
+      }).then((response) => {
+        setListOfPosts(
+          ListOfPosts.map((post) => {
+            if(post.id === postId) {
+              if(response.data.liked) {
+                return {...post, Likes: [...post.Likes, 0]};
+              } else {
+                const likeArray = post.Likes;
+                likeArray.pop();
+                return {...post, Likes: likeArray};
+              }
+            } else {
+              return post;
+            }
+          })
+        );
+
+        if(likedPosts.includes(postId)) {
+          setLikedPosts(likedPosts.filter((id) => {
+            return id != postId;
+          }))
+        } else {
+          setLikedPosts([...likedPosts, postId]);
+        }
+      });
+  }
 
   return (
     <div>
       {ListOfPosts.map((value, key) => {
         return (
-          <div key={key} className='post' onClick={() => {navigate(`/post/${value.id}`)}}> 
+          <div key={key} className='post'> 
             <div className='title'>{value.title} </div>
-            <div className='body'>{value.postText} </div>
-            <div className='footer'>{value.username} </div>
+            <div 
+              className='body'
+              onClick={() => {navigate(`/post/${value.id}`)}}
+            >
+              {value.postText}
+            </div>
+            <div className='footer'>
+              <div className="username">{value.username}</div>
+              <div className="buttons">
+                <img 
+                  className="likeBttn"
+                  src={likedPosts.includes(value.id) ? '../images/unlike.png' : '../images/like.png'}
+                  onClick={() => { likeAPost(value.id) }}
+                />
+
+                <label>{value.Likes.length}</label>
+              </div>
+            </div>
           </div>
         );
       })}
